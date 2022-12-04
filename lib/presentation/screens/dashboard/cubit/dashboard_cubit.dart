5e85d9238d9e09b318c13c863eb7c8edd376d7c4
HosseinBabriani <hossein.babriani@gmail.com>
2022-12-05 02:09:04 +0330

    Multi download isolate

--- lib/presentation/screens/dashboard/cubit/dashboard_cubit.dart
+++ lib/presentation/screens/dashboard/cubit/dashboard_cubit.dart
@@ -1,8 +1,119 @@
+import 'dart:typed_data';
+
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:bloc/bloc.dart';
+import 'package:download_manager/presentation/screens/needed_packages.dart';
 import 'package:equatable/equatable.dart';
 
 part 'dashboard_state.dart';
 
 class DashboardCubit extends Cubit<DashboardState> {
   DashboardCubit() : super(DashboardInitial());
+
+  createFile(int split) async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    var b = BytesBuilder();
+    for (int i = 0; i < split; i++) {
+      final File file = File('${directory.path}/my_file$i.txt');
+      var ress = await file.readAsBytes();
+      b.add(ress);
+    }
+    final File newFile = File('${directory.path}/new_file.zip');
+    await newFile.writeAsBytes(b.toBytes());
+  }
+
+  download(Uri uri, int split) async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    int length = await getUirInfo(uri);
+    var divided = length ~/ split;
+    var lastBuffer = 0;
+    for (int i = 0; i < split; i++) {
+      var port = ReceivePort();
+      var shredderModel = ShredderModel(
+          sendPort: port.sendPort,
+          directory: directory,
+          fromByte: lastBuffer,
+          toByte: divided + lastBuffer,
+          i: i,
+          url: uri);
+
+      if (i != (split - 1)) {
+        await Isolate.spawn(shredder, shredderModel);
+        lastBuffer = (lastBuffer + divided + 1);
+      } else {
+        await Isolate.spawn(shredder, shredderModel);
+      }
+      port.listen((message) {
+        print(message);
+      });
+    }
+  }
+
+  Future<int> getUirInfo(Uri uri) async {
+    var basenameWithoutExtension =
+        path.basenameWithoutExtension(uri.toString());
+    var extension = path.extension(uri.toString());
+    http.Response r = await http.head(uri).timeout(const Duration(seconds: 4));
+    var length = int.parse(r.headers["content-length"].toString());
+    var contentType = r.headers["content-type"].toString();
+    var disposition = r.headers["content-disposition"].toString();
+    return length;
+  }
+
+  static void shredder(ShredderModel shredder) async {
+    final request = http.Request('GET', shredder.url);
+    request.headers.addAll({
+      'range': 'bytes=${shredder.fromByte}-${shredder.toByte}',
+      'cache-control': 'no-cache',
+    });
+    final http.StreamedResponse response = await http.Client().send(request);
+    final contentLength = response.contentLength;
+
+    List<int> bytes = [];
+    response.stream.listen(
+      (List<int> newBytes) {
+        bytes.addAll(newBytes);
+        final downloadedLength = bytes.length;
+        var progress =
+            (downloadedLength.toDouble() / (contentLength ?? 1) * 100).round();
+        shredder.sendPort.send("${shredder.i}=>$progress");
+      },
+      onDone: () async {
+        final File file =
+            File('${shredder.directory.path}/my_file${shredder.i}.txt');
+        await file.writeAsBytes(bytes);
+      },
+      onError: (e) {
+        debugPrint(e);
+      },
+      cancelOnError: true,
+    );
+  }
+
+  static Future<File> getFile(String filename) async {
+    final dir = await getTemporaryDirectory();
+    return File("${dir.path}/$filename");
+  }
+}
+
+class ShredderModel {
+  final SendPort sendPort;
+  final Uri url;
+  final int fromByte;
+  final int toByte;
+  final int i;
+  final Directory directory;
+
+  ShredderModel(
+      {required this.sendPort,
+      required this.url,
+      required this.fromByte,
+      required this.toByte,
+      required this.i,
+      required this.directory});
 }
--- lib/presentation/screens/dashboard/dashboard.dart
+++ lib/presentation/screens/dashboard/dashboard.dart
@@ -1,14 +1,7 @@
-import 'dart:io';
-import 'dart:isolate';
-import 'dart:typed_data';
-
 import 'package:download_manager/presentation/screens/needed_packages.dart';
 import 'package:download_manager/presentation/screens/dashboard/cubit/dashboard_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
-
+import 'dart:math' as math;
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -18,108 +11,56 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  var txte = TextEditingController(
+    text: "https://dl2.soft98.ir/soft/a/AnyDesk.7.1.6.zip"
+  );
+  double _currentSliderValue = 0;
+  int splits = 1;
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<DashboardCubit, DashboardState>(
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
-            appBar: AppBar(
-              title: Text(translate("dashboard.app_bar.title")),
-            ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [CircularProgressIndicator()],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                        onPressed: () async {
-                          var url = Uri.parse(
-                              "https://upload.wikimedia.org/wikipedia/commons/e/e4/GatesofArctic.jpg");
-                          http.Response r = await http.head(url);
-                          var length =
-                              int.parse(r.headers["content-length"].toString());
-                          var contentType =
-                          r.headers["content-type"].toString();
-                          var disposition =
-                          r.headers["content-disposition"].toString();
-                          int len = 5;
-                          print("length: $length");
-                          var divided = length ~/ len;
-                          var arr = List.filled(len, String, growable: false);
-                          var lastBuffer = 0;
-                          int ii = 0;
-                          Future.forEach(arr, (element) async {
-                            ii = ii + 1;
-                            var port = ReceivePort();
-                            if (ii != (len - 1)) {
-                              await Isolate.spawn(spliter,
-                                  [port.sendPort, url, lastBuffer, divided]);
-                              lastBuffer = (lastBuffer + divided + 1);
-                            } else {
-                              await Isolate.spawn(spliter,
-                                  [port.sendPort, url, lastBuffer, length]);
-                            }
-                            // port.listen((message) {
-                            //   if (message != "") {
-                            //     arr.insert(ii, message);
-                            //   }
-                            // });
-                          });
-                        },
-                        child: const Text("Download"))
-                  ],
-                )
-              ],
-            ));
+          appBar: AppBar(
+            title: Text(translate("dashboard.app_bar.title")),
+          ),
+          body: Column(
+            children: [
+              TextField(
+                controller: txte,
+              ),
+              Row(
+                children: [
+                  Slider(
+                    value: _currentSliderValue,
+                    min: 0,
+                    max: 6,
+                    divisions: 6,
+                    label: [1, 2, 4, 8, 16, 24, 32][_currentSliderValue.round()]
+                        .toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValue = value;
+                        splits = [1, 2, 4, 8, 16, 24, 32][_currentSliderValue.round()];
+                      });
+                    },
+                  ),
+                  Text("$splits")
+                ],
+              ),
+              TextButton(onPressed: () {
+                var uri= Uri.parse(txte.text);
+                context.read<DashboardCubit>().download(uri, splits);
+              }, child: Text("Download")),
+              TextButton(onPressed: () {
+                var uri= Uri.parse(txte.text);
+                context.read<DashboardCubit>().createFile(splits);
+              }, child: Text("Create File"))
+            ],
+          ),
+        );
       },
     );
-  }
-
-  static void spliter(List<dynamic> args) async {
-    SendPort sendPort = args[0];
-    Uri url = args[1];
-    int lastBuffer = args[2];
-    int divided = args[3];
-    // var res = await http.get(url, headers: {
-    //   'range': 'bytes=$lastBuffer-${lastBuffer + divided}',
-    //   'cache-control': 'no-cache',
-    // });
-
-    final request = http.Request('GET', url);
-    request.headers.addAll({
-      'range': 'bytes=$lastBuffer-${lastBuffer + divided}',
-      'cache-control': 'no-cache',
-    });
-    final http.StreamedResponse response = await http.Client().send(request);
-    final contentLength = response.contentLength;
-
-    List<int> bytes = [];
-
-    // final file = await getFile('video.mp4');
-
-    response.stream.listen((List<int> newBytes) {
-      bytes.addAll(newBytes);
-      final downloadedLength = bytes.length;
-      downloadedLength.toDouble() / (contentLength ?? 1);
-    },onDone: ()async{
-      print(bytes.length);
-      sendPort.send("bytes");
-      // await file.writeAsBytes(bytes);
-    },onError: (e) {
-      debugPrint(e);
-    },
-      cancelOnError: true,);
-  }
-
-  static Future<File> getFile(String filename) async {
-    final dir = await getTemporaryDirectory();
-    return File("${dir.path}/$filename");
   }
 }
